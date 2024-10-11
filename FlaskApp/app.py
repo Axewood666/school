4ddf2fa2d3deb491930966f4316e58f9e0cc0fd3
Axewood666 <igor.zhugar@mail.ru>
2024-10-11 03:25:27 +0300

    develop: update teacher page

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, Response
 import psycopg2
 
 app = Flask(__name__)
@@ -16,12 +16,6 @@ def main():
 
 @app.route("/teacher")
 def teacher():
-    ''' conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT')
-    users = cur.fetchall()
-    cur.close()
-    conn.close() '''
     return render_template('teacher.html')
 @app.route("/process", methods=['POST'])
 def process():
@@ -35,11 +29,28 @@ def process():
     else:
         cur.execute(f"SELECT * FROM get_class_id_by_teacher_name('{name}','{lastName}')")
     if(cur.rowcount == 0):
-        result = "Преподавателя с таким именем не существует, либо у него нету класса"
+        return 'Преподавателя с таким фио не существует, либо у него нет класса!', 404
     else:
         result = cur.fetchone()
-        cur.execute(f"SELECT * FROM STUDENT WHERE CLASSID={result[0]}")
+        cur.execute(f'''SELECT 
+            s.FirstName,
+            s.MiddleName,
+            s.LastName,
+            s.BirthDate,
+            s.Gender,
+            s.Address,
+            s.PhoneNumber,
+            s.Email,
+            c.ClassName
+        FROM 
+            Student s
+        JOIN 
+            Class c ON s.ClassID = c.ClassID
+        WHERE 
+            s.CLASSID={result[0]}''')
         result = cur.fetchall()
+        headers = tuple([i[0] for i in cur.description])
+        result.insert(0, headers)
     cur.close()
     conn.close()
     return result
--- FlaskApp/static/js/script.js
+++ FlaskApp/static/js/script.js
@@ -11,11 +11,38 @@ function sendData() {
                 '_middleName': middleName
         },
         success: function(response) {
-            document.getElementById('output').innerHTML = response;
+
+            const tbl = document.createElement("table");
+            const tblBody = document.createElement("tbody");
+            let row = document.createElement("tr");
+            for(let i = 0; i < response[0].length; i++){
+                let headCell = document.createElement("th");
+                let  cellText = document.createTextNode(`${response[0][i]}`);
+                headCell.appendChild(cellText);
+                row.appendChild(headCell);
+            }
+            tblBody.appendChild(row);
+            for(let i = 1; i < response.length;i++){
+                row = document.createElement("tr");
+                for (let j = 0; j < response[i].length; j++){
+                    const cell = document.createElement("td");
+                    let cellText = document.createTextNode(`${response[i][j]}`);
+                    cell.appendChild(cellText);
+                    row.appendChild(cell);
+                }
+                tblBody.appendChild(row);
+            }
+            tbl.appendChild(tblBody);
+            const outputElement = document.getElementById("output");
+            while (outputElement.firstChild) {
+              outputElement.removeChild(outputElement.firstChild);
+            }
+            outputElement.appendChild(tbl);
+            tbl.setAttribute("border", "2");
         },
         error: function(error) {
             console.log(error);
-
+            document.getElementById("output").innerHTML = error.responseText;
         }
     });
 }
