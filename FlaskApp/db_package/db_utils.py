f3b2b17aa0d5299633506cfaf4954a5aa83b5c1e
Axewood666 <igor.zhugar@mail.ru>
2024-10-21 18:18:52 +0300

    develop: server bd

--- FlaskApp/db_package/db_utils.py
+++ FlaskApp/db_package/db_utils.py
@@ -7,5 +7,5 @@ config.read(urlconf)
 user_db = config['login_db']['user_db']
 password_db = config['login_db']['password_db']
 name_db = config['login_db']['database_name']
-
-schoolDB = SchoolDB(name_db, user_db, password_db)
+host_db = config['login_db']['host_db']
+schoolDB = SchoolDB(name_db, user_db, password_db, host_db)
